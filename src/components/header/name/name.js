3e6e3d1b874c8b3b86e02bb35aca9544873d8446
Ali ÇİÇEK <alicicek4151@gmail.com>
2023-01-09 14:44:16 +0300

    resume

---
+++ src/components/header/name/name.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./name.css";
+
+const Name = () => {
+    return (
+        <div className="name">
+            <h1>Ali ÇİÇEK</h1>
+            <span>Frontend Developer</span>
+        </div>
+    );
+};
+
+export default Name;
